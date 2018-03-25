4ce3f4e8381ef7e1432b321ff6a7e381e43d125d
renato2110 <renato.mainieri2110@gmail.com>
2018-03-25 15:15:31 -0600

    Prints

--- TP1.c
+++ TP1.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 
 
-int main(int argc,char **argv)
-{
+int main(int argc, char **argv) {
     int numProcess, n, myId;
     double startTime, endTime;
 
-    void generatesMatrix(int m[], int n); //It includes the method
-    void generatesVector(int v[], int n); //It includes the method
+    // They include the methods
+    void generatesMatrix(int m[], int n);
+    void printMatrix(int m[], int n, char name);
+    void generatesVector(int v[], int n);
+    void printVector(int v[], int n, char name);
 
     MPI_Init(&argc, &argv); //It starts MPI
 
@@ -17,17 +19,21 @@ int main(int argc,char **argv)
 
     MPI_Comm_rank(MPI_COMM_WORLD, &myId); //MPI stores in myId the id of the current process
 
-    if(myId == 0){
+    if (myId == 0) {
         //It controls the user cannot enter wrong data
-        do {
+        printf("Type n, which is the dimension of the matrix\n");
+        scanf("%d", &n); //It is the length of each row and the number of rows
+        while (n < numProcess || n % numProcess != 0) {
+            printf("The n must be multiple of the number of processes!!\n");
             printf("Type n, which is the dimension of the matrix\n");
             scanf("%d", &n); //It is the length of each row and the number of rows
-        } while(n<numProcess || n%numProcess!=0);
-
-		int m[n*n], v[n]; //It is the n*n matrix(array) and the n array
-        generatesMatrix(m, n*n);
+        }
+        int m[n * n], v[n]; //It is the n*n matrix(array) and the n array
+        generatesMatrix(m, n * n);
         generatesVector(v, n);
-
+        printMatrix(m, n, 'M');
+        printVector(v, n, 'V');
+        printVector(v, n, 'V');
     } else {
 
     }
@@ -37,20 +43,45 @@ int main(int argc,char **argv)
 }
 
 
-// Method which generates the long vector
-void generatesMatrix(int m[], int n){
+// Method which generates a matrix
+void generatesMatrix(int m[], int n) {
     int i;
     for (i = 0; i < n; i++) {
         m[i] = (int) rand() / (int) (((unsigned) RAND_MAX + 1) / 10);
-        //printf("Soy de 0 a 9\n%d\n",m[i]);
+        // printf("Soy de 0 a 9\n%d\n", m[i]);
     }
 }
 
-// Method which generates the short vector
-void generatesVector(int v[], int n){
+// Method which print a matrix
+void printMatrix(int m[], int n, char name) {
+    int i;
+    int j;
+    int counter;
+    printf("\n-> The Matrix %c", name);
+    for (i = 0; i < n; i++) {
+        printf("\nRow %d:", i + 1);
+        for (j = 0; j < n; j++) {
+            printf(" %d ", m[i + j]);
+        }
+    }
+    printf("\n\n");
+}
+
+// Method which generates a vector
+void generatesVector(int v[], int n) {
     int i;
     for (i = 0; i < n; i++) {
         v[i] = (int) rand() / (int) (((unsigned) RAND_MAX + 1) / 6);
-        //printf("Soy de 0 5\n%d\n",v[i]);
+        // printf("Soy de 0 5\n%d\n", v[i]);
     }
+}
+
+// Method which print a vector
+void printVector(int v[], int n, char name) {
+    int i;
+    printf("\n-> The Vector %c\n", name);
+    for (i = 0; i < n; i++) {
+        printf(" %d ", v[i]);
+    }
+    printf("\n\n");
 }
