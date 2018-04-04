f61c3f10c1a2bb5ac87a37b1a884d484786b93ae
renato2110 <renato.mainieri2110@gmail.com>
2018-04-03 22:24:47 -0600

    Changes

--- TP1.cpp
+++ TP1.cpp
@@ -1,5 +1,5 @@
 /*
-// Created by Kevin Le�n & Renato Mainieri on 25/03/2018.
+// Created by Kevin León & Renato Mainieri on 25/03/2018.
 */
 #include <mpi.h>
 #include <stdio.h>
@@ -10,15 +10,18 @@
 #include <string>
 #include <iostream>
 #include <new>
+#include <ctime>
 
 using namespace std;
+
+#define microSeconds 1000000.0;
 
 class VectorManager {
 public:
 
     void generatesVector(int V[], int n, bool rank);
 
-    void postResults(int M[], int V[], int Q[], int P[], int B[], int n, int numProcesses, int tp, double time,
+    void postResults(int M[], int V[], int Q[], int P[], int B[], int n, int numProcesses, int tp, clock_t startTotalTime,
                      double processTime);
 
     void generatesSendCounts(int sendCounts[], int n_bar, int n);
@@ -26,13 +29,12 @@ public:
     void generatesSendDisplacements(int displs[], int n_bar, int n);
 
     bool isPrime(int value);
-	
-	int obtainNewValue(int M[], int position, int n);
+
+    int obtainNewValue(int M[], int position, int n);
 
 private:
 
-    void writeResults(int M[], int V[], int Q[], int P[], int B[], int n, int numProcesses, int tp, double time,
-                      double processTime);				  
+    void writeResults(int M[], int V[], int Q[], int P[], int B[], int n);
 
     string getMatrix(int M[], int n, string name);
 
@@ -55,13 +57,12 @@ void VectorManager::generatesVector(int V[], int n, bool rank) {
 // Method which post the Results, in console or in a file
 void
 VectorManager::postResults(int M[], int V[], int Q[], int P[], int B[], int n, int numProcesses, int tp,
-                           double totalTime, double processTime) {
+                           clock_t startTotalTime, double processTime) {
     cout << "\nRESULTS\n" << endl;
     cout << "-> Value of n: " << n << endl;
     cout << "-> Total of processes: " << numProcesses << endl;
     cout << "-> Total of prime numbers in M (tp): " << tp << endl;
-    cout << "-> Total time: " << totalTime << endl;
-    cout << "-> Total time, without the times to write results: " << processTime << endl;
+    cout << "-> Total time, without the times to write results: " << processTime << " seconds." << endl;
     if (n <= 100) {
         cout << getMatrix(M, n, "M") << endl;
         cout << getVector(V, n, "V") << endl;
@@ -70,8 +71,12 @@ VectorManager::postResults(int M[], int V[], int Q[], int P[], int B[], int n, i
         cout << getMatrix(B, n, "B") << endl;
         cout << endl;
     } else {
-        writeResults(M, V, Q, P, B, n, numProcesses, tp, totalTime, processTime);
+        writeResults(M, V, Q, P, B, n);
     }
+
+    clock_t endTotalTime = clock();
+    double totalTime = (endTotalTime-startTotalTime)/microSeconds;
+    cout << "-> Total time: " << totalTime << " seconds.\n" << endl;
 
 }
 
@@ -111,8 +116,7 @@ string VectorManager::getMatrix(int M[], int n, string name) {
 
 // Method which generates a new File, with the respective results
 void
-VectorManager::writeResults(int M[], int V[], int Q[], int P[], int B[], int n, int numProcesses, int tp, double time,
-                            double processTime) {
+VectorManager::writeResults(int M[], int V[], int Q[], int P[], int B[], int n) {
     string input;
     input = "RESULTS";
     ofstream out("results.txt");
@@ -167,33 +171,37 @@ bool VectorManager::isPrime(int value) {
     return isPrime;
 }
 
-int VectorManager::obtainNewValue(int M[], int position, int n){
-	int result = 0;
-	result += M[position];  // M[i,j]
-	if ((position%n)>0) { //M[i,j-1], If is not in a left border
-		result += M[position-1];
-	}
-	if (position>=n) {  //M[i-1,j], If is not in the top border
-		result += M[position-n];
- 	}
-	if (position%n!=(n-1)) {  //M[i,j+1], If is not in the right border
-		result += M[position+1];
- 	}
-	if (((n*n)-position)>n) {  //M[i+1,j], If is not in the down border
-		result += M[position+n];
- 	}
-	return result;
+int VectorManager::obtainNewValue(int M[], int position, int n) {
+    int result = 0;
+    result += M[position];  // M[i,j]
+    if ((position % n) > 0) { //M[i,j-1], If is not in a left border
+        result += M[position - 1];
+    }
+    if (position >= n) {  //M[i-1,j], If is not in the top border
+        result += M[position - n];
+    }
+    if (position % n != (n - 1)) {  //M[i,j+1], If is not in the right border
+        result += M[position + 1];
+    }
+    if (((n * n) - position) > n) {  //M[i+1,j], If is not in the down border
+        result += M[position + n];
+    }
+    return result;
 }
 
 
 int main(int argc, char **argv) {
+    clock_t startTotalTime;
+    clock_t endTotalTime;
+
     int numProcesses, n, myId, tp, qCounter, bCounter, tempNBar, i;
-    int n_bar;        /*  Se calculara como  n/p, es decir es el numero de
-                           elementos que le corresponde a cada proceso de cada vector */
-    double startTime, endTotalTime, endProcessTime; // MPI_Wtime()
+    int n_bar;
+    double startMPITime, endMPITime;
     int *M, *B, *localM, *localB, *V, *Q, *P, *localQ, *displacements, *sendCounts;
 
     VectorManager vectorManager;
+
+
 
     MPI_Init(&argc, &argv); //It starts MPI
 
@@ -202,7 +210,7 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myId); //MPI stores in myId the id of the current process
 
     if (myId == 0) {
-
+        startTotalTime = clock();
         //It controls the user cannot enter wrong data
         cout << "\nType n, which is the dimension of the matrix" << endl;
         cin >> n; //It is the length of each row and the number of rows
@@ -211,6 +219,8 @@ int main(int argc, char **argv) {
             cout << "Type n, which is the dimension of the matrix." << endl;
             cin >> n; //It is the length of each row and the number of rows
         }
+        startMPITime = MPI_Wtime();
+
         M = new(nothrow) int[n * n];
         B = new(nothrow) int[n * n];
         Q = new(nothrow) int[n];
@@ -219,7 +229,7 @@ int main(int argc, char **argv) {
         sendCounts = new(nothrow) int[numProcesses];
 
         if (M == NULL || B == NULL || Q == NULL || V == NULL || displacements == NULL || sendCounts == NULL) {
-            cout << "No se ha encontrado memoria disponible" << endl;
+            cout << "No available memory found!!" << endl;
             return 0;
         }
 
@@ -248,14 +258,13 @@ int main(int argc, char **argv) {
     localQ = new(nothrow) int[n_bar];
 
     if (P == NULL || localM == NULL || localB == NULL || localQ == NULL || V == NULL) {
-        cout << "No se ha encontrado memoria disponible" << endl;
+        cout << "No available memory found!!" << endl;
         return 0;
     }
 
-    for (int k = 0; k < n; ++k) {
+    for (int k = 0; k < n; ++k) {    // Initialize the P content
         P[k] = 0;
     }
-
 
     MPI_Scatter(Q, n_bar, MPI_INT, localQ, n_bar, MPI_INT, 0,
                 MPI_COMM_WORLD); // It sends to each process a part of Q vector for the multiplication
@@ -266,7 +275,7 @@ int main(int argc, char **argv) {
     tp = 0;
     i = 0;
     qCounter = 0;
-	bCounter = 0;
+    bCounter = 0;
     tempNBar = 0;
     if (myId > 0) {
         ++i;
@@ -280,17 +289,17 @@ int main(int argc, char **argv) {
                 ++tp;
             }
             column += localM[i * n + j] * V[j];
-			localB[bCounter] = vectorManager.obtainNewValue(localM, i*n+j, n);
-			++bCounter;
+            localB[bCounter] = vectorManager.obtainNewValue(localM, i * n + j, n);
+            ++bCounter;
         }
         localQ[qCounter] = column;
         ++qCounter;
-		
+
     }
 
     MPI_Gather(localQ, n_bar, MPI_INT, Q, n_bar, MPI_INT, 0, MPI_COMM_WORLD);
 
-    MPI_Gather(localB, n_bar*n, MPI_INT, B, n_bar*n, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(localB, n_bar * n, MPI_INT, B, n_bar * n, MPI_INT, 0, MPI_COMM_WORLD);
 
     MPI_Reduce(P, P, n, MPI_INT, MPI_SUM, 0,
                MPI_COMM_WORLD); // It completes the P vector, adding to each column the quantity of prime numbers per column
@@ -298,8 +307,8 @@ int main(int argc, char **argv) {
                MPI_COMM_WORLD); // It completes tp, adding the quantity of primes numbers.
 
     if (myId == 0) {
-        vectorManager.postResults(M,V, Q, P, B, n, numProcesses, tp, 0, 0);
-
+        endMPITime = MPI_Wtime();
+        vectorManager.postResults(M, V, Q, P, B, n, numProcesses, tp, startTotalTime, endMPITime-startMPITime);
         delete[] M, B, Q, V, sendCounts, displacements;
     }
 
